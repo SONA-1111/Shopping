d49d5929f076091a67e0104eeff5f918ae9cced4
Sona Jaiswal <114572821+SONA-1111@users.noreply.github.com>
2023-02-22 10:13:56 +0545

    update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from path;
+import path from "path";
 import {fileURLToPath} from 'url';
 //configure env
 dotenv.config();
